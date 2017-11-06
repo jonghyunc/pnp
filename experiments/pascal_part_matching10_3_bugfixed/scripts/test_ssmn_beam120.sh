3505a9c27756f02b9df112b790463a134caf77d2
jonghyunc <jonghyunc@allenai.org>
2017-11-06 11:16:56 -0800

    ppm10_3 test_beam120 skipping val

--- experiments/pascal_part_matching10_3_bugfixed/scripts/test_ssmn_beam120.sh
+++ experiments/pascal_part_matching10_3_bugfixed/scripts/test_ssmn_beam120.sh
@@ -18,9 +18,9 @@ mkdir -p $TEST_DIR
 # echo "Training $MY_NAME model..."
 # ./$SCRIPT_DIR/run.sh org.allenai.dqa.matching.TrainMatchingCli --beamSize $TRAIN_BEAM --epochs $EPOCHS --examples $TRAIN --diagrams $DIAGRAMS --diagramFeatures $DIAGRAM_FEATURES --modelOut $MY_MODEL $TRAIN_OPTS $MY_FLAGS > $MY_DIR/log.txt
 
-echo "Testing $MY_NAME model on validation..."
-VAL="$DATA_DIR/data_splits/$DATA_SPLIT/validation.json"
-./$SCRIPT_DIR/run.sh org.allenai.dqa.matching.TestMatchingCli --beamSize $TEST_BEAM --examples $VAL --diagrams $DIAGRAMS --diagramFeatures $DIAGRAM_FEATURES --model $MY_MODEL --lossJson $TEST_DIR/validation_error.json  > $TEST_DIR/validation_error_log.txt
+#echo "Testing $MY_NAME model on validation..."
+#VAL="$DATA_DIR/data_splits/$DATA_SPLIT/validation.json"
+#./$SCRIPT_DIR/run.sh org.allenai.dqa.matching.TestMatchingCli --beamSize $TEST_BEAM --examples $VAL --diagrams $DIAGRAMS --diagramFeatures $DIAGRAM_FEATURES --model $MY_MODEL --lossJson $TEST_DIR/validation_error.json  > $TEST_DIR/validation_error_log.txt
 
 echo "Testing $MY_NAME model on test..."
 TEST="$DATA_DIR/data_splits/$DATA_SPLIT/test.json"
